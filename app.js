19fac810dad86f7268eadcd1b6172f52815fd285
weiwu <645046307@qq.com>
2017-04-08 11:36:50 +0800

    添加组件

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const path = require('path')
-const pkg = require('package.json')
+const pkg = require('./package.json')
+const config = require('./config/default')
+const routes = require('./routes')
+
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
 const express = require('express')
 const app = express()
-const router = require('./routes')(app)
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(session({
+  name: config.session.key,
+  secret: config.session.secret,
+  resave: true,
+  cookie: {
+    maxAge: config.session.maxAge
+  },
+  store: new MongoStore({
+    url: config.mongodb
+  })
+}))
+
+routes(app)
 
 const port = process.env.PORT || config.port
 app.listen(port, () => {
