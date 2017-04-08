0f068d877f9acac857486e2e1d83bbe4eb5784e8
weiwu <645046307@qq.com>
2017-04-08 15:51:38 +0800

    获取并整理新闻页的数据

--- crawlers/trendCrawler.js
+++ crawlers/trendCrawler.js
@@ -1,7 +1,5 @@
 const request = require('superagent')
-const cheerio = require('cheerio')
 
-const getNews = require('./trendNewsCrawler')
 const trend = require('../config/default').xtuUrl.trend
 
 module.exports = function (target, cb) {
@@ -10,14 +8,7 @@ module.exports = function (target, cb) {
   request
     .get(url)
     .end((err, sres) => {
-      if (err) { throw new Error('获取新闻资讯失败') }
-
-      console.log(sres.text)
-
-      switch (target) {
-        case 'news':
-          return getNews(sres.text)
-          break
-      }
+      if (err) { throw new Error('获取动态失败') }
+      cb(sres.text)
     })
 }
--- routes/v1/trend/news.js
+++ routes/v1/trend/news.js
@@ -1,10 +1,26 @@
+const fs = require('fs')
+const request = require('superagent')
+const cheerio = require('cheerio')
+
 const trendCrawler = require('../../../crawlers/trendCrawler')
 
 module.exports = function (req, res) {
-  trendCrawler('news', function (data) {
+  trendCrawler('news', function (html) {
+    // fs.writeFile(__dirname + '/news.html', data)
 
+    let $ = cheerio.load(html),
+        ret = [],
+        cur = $('.list a')
+
+    for (let i = 0, len = cur.length; i < len; i++) {
+      let temp = {},
+          $a = $(cur[i])
+
+      temp.href = $a.attr('href')
+      temp.title = $a.attr('title')
+      temp.time = $a.find('span').text()
+      ret.push(temp)
+    }
+    res.status(200).send(ret)
   })
-
-
-  res.status(200).send('news')
 }
