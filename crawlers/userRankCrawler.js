d6837c75d026d3be3eff41bd3a307898c30c50fa
weiwu <645046307@qq.com>
2017-04-10 21:53:57 +0800

    增加查询排名的功能

--- crawlers/userRankCrawler.js
+++ crawlers/userRankCrawler.js
@@ -1,52 +1,60 @@
 const request = require('superagent')
-const charset = require('superagent-charset')
-charset(request)
 const cheerio = require('cheerio')
+const eventproxy = require('eventproxy')
 
 const header = require('../config/default').header
 const user = require('../config/default').xtuUrl.user
 
 module.exports = (req, res) => {
-  // let url = 'http://jwxt.xtu.edu.cn/jsxsd/kscj/cjjd_list'
   const rankUrl = user.host + user.path.rank
-  const PROP = ['all', '7', '1']
+  const YEAR = [
+    '2014-2015-1',
+    '2014-2015-2',
+    '2015-2016-1',
+    '2015-2016-2',
+    '2016-2017-1'
+  ]
+  const prop = ['all', '7', '1']
 
-  const year = req.body.year || '2016-2017-1'
-  const nextYear = year + 1
-  const half = req.body.half || 1
-  const data = year + '-' + nextYear + '-' + half
+  let fullYear = req.body.fullYear || '2016-2017-1'
+  fullYear.indexOf('&') > -1 && (year = fullYear.replace(/&/g, '&kksj='))
 
-  const prop = PROP[req.body.prop || 0] // 1:必修 or 2:选修 or all: 综合
+  let ep = new eventproxy()
+  ep.after('getHtml', prop.length, (htmlArr) => {
+    let result = []
+    htmlArr.map((htmlObj) => {
+      let { html, propEl } = htmlObj,
+          $ = cheerio.load(html),
+          obj = {},
+          table = []
 
-  request
-    .post(rankUrl)
-    .set(header)
-    .set('Cookie', req.session.xtu)
-    // .send({ kksj: yearEl, kclb: propEl, zsb: 0 })
-    .send(`kksj=${ data }&kclb=${ prop = prop !== 'all' ? prop : '1&kclb=7' }&zsb=0`)
-    // .charset('utf-8')
-    .end((err, sres) => {
-      if (err) { throw new Error('获取排名失败') }
+      obj.year = fullYear
+      if (propEl.length === 1) { obj.prop = propEl === '1' ? '必修' : '选修' }
+      else { obj.prop = '综合' }
 
-      // let $ = cheerio.load(sres.text),
-      //     obj = {},
-      //     table = []
-
-      // obj.year = data
-      // if (prop.length === 1) { obj.prop = prop === '1' ? '必修' : '选修' }
-      // else { obj.prop = '综合' }
-
-      // $('#dataList tr').find('th').each((i, th) => {
-      //   table[i] = {}
-      //   table[i].title = $(th).text()
-      // })
-      // $('#dataList tr').find('td').each((i, td) => {
-      //   table[i].num = $(td).text()
-      // })
-      // // console.log(table)
-      // obj.table = table
-      // console.log(obj)
+      $('#dataList tr').find('th').each((i, th) => {
+        table[i] = {}
+        table[i].title = $(th).text()
+      })
+      $('#dataList tr').find('td').each((i, td) => {
+        table[i].num = $(td).text()
+      })
+      obj.table = table
       result.push(obj)
     })
+    result.sort((a, b) => a.year > b.year ? 1 : -1)
+    console.log('=== 获取排名成功 ===')
+    res.status(200).json(result)
+  })
 
+  prop.map((propEl, pid) => {
+    request.post(rankUrl)
+      .set(header)
+      .set('Cookie', req.session.xtu)
+      .send(`kksj=${ year }&kclb=${ propEl = propEl !== 'all' ? propEl : '1&kclb=7' }&zsb=0`)
+      .end((err, sres) => {
+        if (err) { throw new Error(`获取排名失败 ${err}`) }
+        ep.emit('getHtml', { html: sres.text, propEl })
+      })
+  })
 }
