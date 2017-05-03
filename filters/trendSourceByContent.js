6186a1ecfd2ca4beb39be150d40ab29e22dd6bdc
weiwu <645046307@qq.com>
2017-05-04 01:09:10 +0800

    修复数据来源中多余空格的问题

--- filters/trendSourceByContent.js
+++ filters/trendSourceByContent.js
@@ -4,7 +4,7 @@ module.exports = detail => {
 
   for (let i = content.length - 1; i >= 0 && !isFound; i--) {
     if (content[i].includes('年')) {
-      detail.source = content[i - 1]
+      detail.source = content[i - 1].replace(/\s+/g, '')
       isFound = true
     } else if (content[i].includes('来源：')) {
       content[i].replace(/：(.*)）/g, (match, g1) => {
