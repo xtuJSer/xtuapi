a647e1e4fe6528c1d0b47a6c5b07753963dca4a9
weiwu <645046307@qq.com>
2017-05-04 00:34:06 +0800

    修复获取“媒体”来源异常的问题

--- filters/trendSourceByTitle.js
+++ filters/trendSourceByTitle.js
@@ -1,9 +1,10 @@
 module.exports = detail => {
   // const END = detail.title.indexOf('】')
   // detail.source = detail.title.substring(1, END)
-  let section = detail.title.split('】')
-  detail.source = section[0].substring(1)
-  detail.title = section[1]
+  // let section = detail.title.split('】')
+  let pos = detail.title.indexOf('】')
+  detail.source = detail.title.substring(1, pos)
+  detail.title = detail.title.substring(pos + 1)
 
   return detail
 }
