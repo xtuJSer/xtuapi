2ea8b7c17d0efcf5f314c609011db7bec3feb4d9
weiwu <645046307@qq.com>
2017-05-08 11:56:57 +0800

    增加对获取空闲教室的用户识别

--- routes/v1/user/classroom.js
+++ routes/v1/user/classroom.js
@@ -1,6 +1,12 @@
 const userClassroomCrawler = require('../../../crawlers/userClassroomCrawler')
 
-module.exports = async (req, res) => {
-  let session = await require('./login-Alpha')(req, res, false)
-  await userClassroomCrawler(req, res, session)
+module.exports = async (req, res, isUser = true) => {
+  if (isUser) {
+    let session = await require('./login-Alpha')(req, res, false)
+    await userClassroomCrawler(req, res, session)
+  } else {
+    let day = req.body.day || 0
+    ;(day < 0 || day > 1) && (day = 0)
+    res.status(200).json(require('../../../store/classroom_' + day + '.json'))
+  }
 }
