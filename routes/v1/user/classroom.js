379784edd98a03219f1ff4a2b2d39fc81ec35b59
weiwu <645046307@qq.com>
2017-05-09 10:39:42 +0800

    提供机器人访问接口

--- routes/v1/user/classroom.js
+++ routes/v1/user/classroom.js
@@ -1,8 +1,12 @@
 const userClassroomCrawler = require('../../../crawlers/userClassroomCrawler')
 const { judgeDay } = require('../../../filters/userClassroom')
 
-module.exports = async (req, res, isUser = true) => {
-  if (isUser) {
+module.exports = async (req, res) => {
+  let isRobot = req.body.isRobot || 0
+  if (+isRobot === 1) {
+    let session = await require('./login-Alpha')(req, res, false)
+    await userClassroomCrawler(req, res, session)
+  } else {
     let day = +req.body.day || 0,
         byName = +req.body.byName || 0
 
@@ -10,8 +14,5 @@ module.exports = async (req, res, isUser = true) => {
     ;(byName < 0 || byName > 1) && (byName = 0)
 
     res.status(200).json(require('../../../store/classroom_' + (byName ? 'name' : 'time') + '_' + judgeDay(day) + '.json'))
-  } else {
-    let session = await require('./login-Alpha')(req, res, false)
-    await userClassroomCrawler(req, res, session)
   }
 }
