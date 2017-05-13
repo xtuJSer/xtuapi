0d55085cd462126a06c36ffc67d9ab57c08249e4
weiwu <645046307@qq.com>
2017-05-13 08:09:36 +0800

    使用四个时间点获取数据增加数据可靠性

--- robot/userClassroomRobot.js
+++ robot/userClassroomRobot.js
@@ -3,21 +3,24 @@ const request = require('superagent')
 
 const URL = 'https://xtuapi.magicallu.cn/v1/user/classroom'
 // const URL = 'http://localhost:3000/v1/user/classroom'
+const updateTime = ['11 11 0 * * *', '22 22 0 * * *', '5 5 5 * * *', '6 6 6 * * *']
 
 const getDay = () => {
-  schedule.scheduleJob('11 11 0 * * *', () => {
-    for (let i = 0; i <= 1; i++) {
-      request.post(URL)
-        .send({
-          byName: 1,
-          day: 1,
-          isRobot: 1
-        })
-        .end((err, data) => {
-          console.log(`${Date.now()} 数据byName: ${i} 已更新`)
-        })
-    }
-  })
+  for (let el of updateTime) {
+    schedule.scheduleJob(el, () => {
+      for (let i = 0; i <= 1; i++) {
+        request.post(URL)
+          .send({
+            byName: 1,
+            day: 1,
+            isRobot: 1
+          })
+          .end((err, data) => {
+            console.log(`${Date.now()} 数据byName: ${i} 已更新`)
+          })
+      }
+    })
+  }
 }
 
 getDay()
