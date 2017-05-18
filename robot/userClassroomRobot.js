d287263c1c93f99c0c8b30213f6c3f950d3e9880
weiwu <645046307@qq.com>
2017-05-18 18:55:44 +0800

    完善获取课程机器人，引用async

--- robot/userClassroomRobot.js
+++ robot/userClassroomRobot.js
@@ -3,27 +3,33 @@ const request = require('superagent')
 
 const baseURL = require('../config/default').xtuURL.user
 const URL = baseURL.host + baseURL.path.classroom
-
 const updateTime = ['5 0 0 * * *', '11 11 0 * * *', '22 22 0 * * *', '5 5 5 * * *', '6 6 6 * * *']
 
-const getDay = () => {
-  for (let el of updateTime) {
-    schedule.scheduleJob(el, () => {
+const fetchData = (day, byName) => {
+  return new Promise((resolve, reject) => {
+    request.post(URL)
+      .send({
+        byName,
+        day,
+        isRobot: 1
+      })
+      .end((err, data) => {
+        if (err) { reject(err) }
+
+        console.log(`${new Date()} userClassroomRobot 更新数据 byName: ${byName}, day: ${day}`)
+        resolve()
+      })
+  })
+}
+
+;(() => {
+  for (let time of updateTime) {
+    schedule.scheduleJob(time, async () => {
       for (let i = 0; i <= 1; i++) {
         for (let j = 0; j <= 1; j++) {
-          request.post(URL)
-            .send({
-              byName: i,
-              day: j,
-              isRobot: 1
-            })
-            .end((err, data) => {
-              console.log(`${Date.now()} userClassroomRobot 更新数据 byName: ${i}, day: ${j}`)
-            })
+          await fetchData(i, j)
         }
       }
     })
   }
-}
-
-getDay()
+})()
