afcd773b6fbb13c6aadf6bfe88996c505be399b9
weiwu <645046307@qq.com>
2017-06-15 23:24:50 +0800

    确认已更新日志

--- robot/userClassroomRobot.js
+++ robot/userClassroomRobot.js
@@ -1,8 +1,10 @@
+const fs = require('fs')
 const schedule = require('node-schedule')
 const request = require('superagent')
 
 const URL = 'https://xtuapi.magicallu.cn/v1/user/classroom'
-const updateTime = ['11 0 0 * * *', '11 11 0 * * *', '22 22 0 * * *', '5 5 5 * * *', '6 6 6 * * *']
+const updateTime = ['10 0 0 * * *', '10 10 0 * * *', '21 21 0 * * *', '5 5 5 * * *', '6 6 6 * * *']
+const confirmTime = ['30 0 0 * * *', '11 11 0 * * *', '22 22 0 * * *', '50 5 5 * * *', '50 6 6 * * *']
 
 const fetchData = (day, byName) => new Promise((resolve, reject) => {
   request.post(URL)
@@ -27,6 +29,12 @@ const fetchData = (day, byName) => new Promise((resolve, reject) => {
           await fetchData(i, j)
         }
       }
+    })
+  }
+
+  for (let time of confirmTime) {
+    schedule.scheduleJob(time, () => {
+      fs.writeFile('./confirmTime.txt', '滴，学生卡', () => console.log(`${new Date()} confirmTime`))
     })
   }
 })()
