dbb5993388b3cb3c3803a2cd3ef83fc0f917ffbc
weiwu <645046307@qq.com>
2017-09-16 17:42:31 +0800

    rm

--- middlewares/checkLogin.js
+++
@@ -1,9 +0,0 @@
-module.exports = target => {
-  return (req, res, next) => {
-    console.log(target, req.session[target])
-    if (!req.session[target]) {
-      return res.status(500).send('请先登录')
-    }
-    next()
-  }
-}
