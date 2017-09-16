8df0b5b0b660058f4098bd9d6c92b4ac581a9cb4
weiwu <645046307@qq.com>
2017-09-16 17:38:19 +0800

    merge

--- app.js
+++ app.js
@@ -8,15 +8,4 @@ app.use(async (ctx) => {
   ctx.body = 'Hello v2.0'
 })
 
-<<<<<<< HEAD
-routes(app)
-
-module.exports = app
-
-const port = process.env.PORT || config.port
-app.listen(port, () => {
-  console.log(`Server is listening at port ${port}...`)
-})
-=======
 app.listen(3000)
->>>>>>> dev
