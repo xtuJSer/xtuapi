955a179719863cfe3972efd1da6650bf5ca99e35
lulu <522413622@qq.com>
2018-01-12 23:49:18 +0800

    后续规范测试用例

--- test/blog.test.js
+++ test/blog.test.js
@@ -7,12 +7,12 @@ const testRequest = request(server)
 const { blog: { scopes } } = require('../config')
 const target = 'blog'
 
-describe(`\n---- #TEST ${target} ----`, () => {
+describe(`#TEST ${target}`, () => {
   scopes.map(scope => {
     const scopeName = scope[0]
     const topics = Object.keys(scope[1]).filter(key => key !== 'host')
 
-    describe(`\n=== scope: ${scopeName} ===`, () => {
+    describe(`scope: ${scopeName}`, () => {
       topics.map(topic => {
         const fullPath = `/${target}/${scopeName}/${topic}`
 
