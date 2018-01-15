c8e144fac70b932c6f1243782d2ad8996ff1c73a
lulu <522413622@qq.com>
2018-01-15 22:31:00 +0800

    添加用户测试

--- test/user.test.js
+++ test/user.test.js
@@ -3,27 +3,79 @@ const request = require('supertest')
 const app = require('../app')
 const server = app.listen(3030)
 const testRequest = request(server)
-// const testRequest = request.agent(server)
 
+const headers = {
+  accept: 'application/json'
+}
 const loginData = require('../config/private') || {}
-const testFn = function (method, url, done) {
+const testFn = function ({ method = 'get', url, data = null, status = 200 }, done) {
   return testRequest[method](url)
-    .set('Accept', 'application/json')
-    .send(loginData)
-    .expect(200)
-    .end((err, res) => {
-      if (err) {
-        throw new Error(err)
-      }
-
-      return done(res)
+    .set(headers)
+    .send(data)
+    .expect(status)
+    .then(res => {
+      return Promise.resolve(
+        done ? done(res) : {}
+      )
+    })
+    .catch(err => {
+      return Promise.reject(err)
     })
 }
 
 describe(`#TEST User`, () => {
-  it('post login', (done) => {
-    testFn('post', '/user/login', (res) => {
-      console.log(res.body.token)
+  it('【登录】正常登录', async () => {
+    await testFn({
+      method: 'post',
+      url: '/user/login',
+      data: loginData
+    }, (res) => {
+      headers.authorization = `Bearer ${res.body.token}`
+    })
+  })
+
+  it('【登录】账号密码为空', async () => {
+    await testFn({
+      method: 'post',
+      url: '/user/login',
+      status: 401,
+      data: {
+        username: '',
+        password: ''
+      }
+    })
+  })
+
+  it('【登录】账号不符合规范', async () => {
+    await testFn({
+      method: 'post',
+      url: '/user/login',
+      status: 401,
+      data: {
+        ...loginData,
+        username: '2014'
+      }
+    })
+  })
+
+  it('【登录】密码错误', async () => {
+    await testFn({
+      method: 'post',
+      url: '/user/login',
+      status: 401,
+      data: {
+        ...loginData,
+        password: '12345678'
+      }
+    })
+  })
+
+  it('【登录】方法错误', async () => {
+    await testFn({
+      method: 'get',
+      url: '/user/login',
+      status: 404,
+      data: loginData
     })
   })
 })
