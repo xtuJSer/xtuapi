54a490cd3ee24d80dabb0dae80ec459357000a37
lulu <522413622@qq.com>
2018-01-20 23:26:29 +0800

    添加类型

--- src/models/classroom.ts
+++ src/models/classroom.ts
@@ -6,17 +6,22 @@ const ClassroomSchema = new Schema({
   data: Schema.Types.Mixed
 })
 
+type TYPE = {
+  day: number,
+  data: string
+}
+
 /**
  * 通过 day 判断更新对象，并返回
  */
-ClassroomSchema.statics.updateByDay = async function ({ data, day = 0 }: { data: string, day: number }) {
+ClassroomSchema.statics.updateByDay = async function ({ data, day = 0 }: TYPE) {
   await this.findOneAndUpdate({ day }, { $set: { data } }).exec()
 }
 
 /**
  * 按需获取数据库中的数据
  */
-ClassroomSchema.statics.getByDay = async function ({ day = 0 }) {
+ClassroomSchema.statics.getByDay = async function ({ day = 0 }: TYPE) {
   let ret = await this.findOne({ day }, { data: 1, _id: 0 }).exec()
 
   return ret
