66f4678885b2404bb14b707af0e056f890f8b52f
lulu <522413622@qq.com>
2018-02-05 15:05:08 +0800

    新增：用于获取混淆码的地址配置

--- src/config/user.ts
+++ src/config/user.ts
@@ -8,6 +8,7 @@ export default {
     host: 'http://jwxt.xtu.edu.cn/jsxsd',
     path: {
       login: '/xk/LoginToXk',
+      encoded: '?flag=sess',
       verification: '/verifycode.servlet',
       course: '/kscj/cjcx_list?xq=',
       schedule: '/xskb/xskb_list.do',
