be62c6bf293495a0efaafe9f64200fb0dff6da72
lulu <522413622@qq.com>
2018-04-01 23:27:52 +0800

    新增：根路径下的反馈

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,6 +4,9 @@ const router = new KoaRouter()
 import user from './user'
 import blog from './blog'
 
+router.get('/', async (ctx) => {
+  ctx.body = "https://github.com/xtuJSer/xtuapi"
+})
 router.use('/user', user.routes(), user.allowedMethods())
 router.use('/blog', blog.routes(), blog.allowedMethods())
 
