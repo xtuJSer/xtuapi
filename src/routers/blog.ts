5ac96aa5a62adf1ce1ce510bc6b9640611af6941
lulu <522413622@qq.com>
2018-04-02 21:58:25 +0800

    恢复：根路径下的 api 提示

--- src/routers/blog.ts
+++ src/routers/blog.ts
@@ -2,7 +2,7 @@ import * as controller from '../controllers/blog'
 import * as KoaRouter from 'koa-router'
 const router = new KoaRouter()
 
-// router.get('/', controller.getBlogApi)
+router.get('/', controller.getBlogApi)
 router.post('/', controller.getBlog)
 router.get('/:scope/:topic', controller.updateBlog)
 router.get('/dict', controller.getBlogDict)
