34b2f2df28e2cccc675f141d58ef872c30d3a958
lulu <522413622@qq.com>
2018-04-12 16:33:00 +0800

    完善

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -1,9 +1,7 @@
 import app from '../app'
 import config from '../config'
 
-const { port } = config
-
-app.listen(port)
+app.listen(config.port)
 
 console.clear()
-console.log(`App Started At Port ${port}...`)
+console.log(`App Started At Port ${config.port}...`)
