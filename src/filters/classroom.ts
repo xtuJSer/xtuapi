0404529db7e82d1969bafbcf59779ea8d2400c93
lulu <522413622@qq.com>
2018-05-06 09:17:42 +0800

    新增：教务系统 id

--- src/filters/classroom.ts
+++ src/filters/classroom.ts
@@ -1,3 +1,21 @@
+const ID_LIST = [
+  { name: '逸夫楼', id: '01' },
+  { name: '一教楼', id: '02' },
+  { name: '兴湘学院三教', id: '03' },
+  { name: '文科楼', id: '04' },
+  { name: '外语楼', id: '05' },
+  { name: '土木楼', id: '06' },
+  { name: '经管楼', id: '07' },
+  { name: '法学楼', id: '08' },
+  { name: '法学附', id: '09' },
+  { name: '南山阶梯', id: '10' },
+  { name: '北山阶梯', id: '11' },
+  { name: '工科楼阶梯', id: '12' },
+  { name: '图书馆南', id: '13' },
+  { name: '体育馆', id: '14' },
+  { name: '计算中心', id: '15' },
+]
+
 const list = {
   classroomList: [
     '经管',
@@ -41,25 +59,32 @@ const _classroomGetNameAndRoom = name => {
   return { curName, nextName }
 }
 
+const _sortDataById = data => {
+  return data.sort((a, b) => a.id - b.id)
+}
+
 const classroomFormat = (data) => {
-  let Name = {}
+  let item = {}
   data.map((el, idx) => {
     let { classroomName: name, classroomTime: time } = el
     let { curName, nextName } = _classroomGetNameAndRoom(name)
 
-    Name[curName] || (Name[curName] = [])
-    Name[curName].push({ room: nextName, time })
+    item[curName] || (item[curName] = [])
+    item[curName].push({ room: nextName, time })
   })
 
-  let ret = []
-  Object.keys(Name).forEach(key => {
-    ret.push({
+  let ret:object[] = []
+  Object.keys(item).forEach(key => {
+    const id_item = ID_LIST.find(el => el.name === key)
+
+    id_item && ret.push({
+      id: id_item.id,
       building: key,
-      rooms: Name[key]
+      rooms: item[key]
     })
   })
 
-  return ret
+  return _sortDataById(ret)
 }
 
 export default {
