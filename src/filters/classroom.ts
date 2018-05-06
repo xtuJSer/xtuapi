b3a65839e69664e496dd5d6e426f2d80873aa263
lulu <522413622@qq.com>
2018-05-06 15:10:25 +0800

    新增：空闲教室 id

--- src/filters/classroom.ts
+++ src/filters/classroom.ts
@@ -1,20 +1,6 @@
-const ID_LIST = [
-  { name: '逸夫楼', id: '01' },
-  { name: '一教楼', id: '02' },
-  { name: '兴湘学院三教', id: '03' },
-  { name: '文科楼', id: '04' },
-  { name: '外语楼', id: '05' },
-  { name: '土木楼', id: '06' },
-  { name: '经管楼', id: '07' },
-  { name: '法学楼', id: '08' },
-  { name: '法学附', id: '09' },
-  { name: '南山阶梯', id: '10' },
-  { name: '北山阶梯', id: '11' },
-  { name: '工科楼阶梯', id: '12' },
-  { name: '图书馆南', id: '13' },
-  { name: '体育馆', id: '14' },
-  { name: '计算中心', id: '15' },
-]
+import config from '../config/classroom'
+
+const { ID_LIST } = config
 
 const list = {
   classroomList: [
